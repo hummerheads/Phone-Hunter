297513d9ec75a42b9c9ad8b2e0145eda6f1bcfd7
Abdullah Al Mamun <101423827+hummerheads@users.noreply.github.com>
2023-09-01 15:29:41 +0600

    Miss

--- js/api.js
+++ js/api.js
@@ -38,3 +38,30 @@ const displayPhones = phones => {
     })
 }
 Load();
+
+const dreamGirl = [
+    {
+        sokina: {
+            name: "bbu",
+            height: "5.4",
+            family: [
+                { father: "rock", mother: "shila", sister: "chinki" }
+            ],
+            age: undefined,
+            contactInfo: [
+                        {
+                            facebook: {
+                            link: "https://www.facebook.com/",
+                            followers: "12545",
+                            status: "single",
+                            friendsList: [{ name: "rofik" }, undefined],
+                            },
+                        },
+                        { 
+                            instagram: "https://www.instagram.com/" 
+                        },
+                        ],
+        },
+    },
+   ];
+   
